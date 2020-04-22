1cae75cc5e43c33b883e9f83d25d52b542784dd9
Sai Krishna Kalakonda <saikrishna.kalakonda@ibm.com>
2020-04-22 03:25:01 -0400

    get method

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const users = [
+    {
+        id: 1,
+        name: 'Sai'
+    },
+    {
+        id: 2,
+        name: 'Krishna'
+    },
+    {
+        id: 3,
+        name: 'Charlie'
+    }
+];
 
 app.get('/', function(req, res) {
     res.send('Hello World');
+})
+
+app.get('/users', function(req, res) {
+    res.send(users);
+})
+
+app.get('/users/:id', function(req, res) { // we can use many url params like /users/:id/:date
+    //const user = users.find(user => user.id == parseInt(req.params.id))  //req.params.id is the one which we are passing from above line.
+    //res.send(user);
+
+    //if query is passed in the url like http://localhost:3000/users/1?name=Sai
+    res.send(req.query.name) // we can get the query params like req.query.name
 })
 
 app.listen(3000, function() {
